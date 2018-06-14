f371d828b581940c2179b4cbb84ddf8e3b9fb449
chriskelsey <chriskelsey.net@gmail.com>
2018-06-13 21:26:06 -0700

    Daily Push

---
+++ assets/javascript/game.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+	// Declare global variables
+	var wins = 0;
+	var loss = 0;
+	var answer = 0;
+	var guessTotal = 0;
+
+	function randNumGen() {
+		answer = Math.floor(Math.random() * 102) + 19;
+		guessTotal = 0;
+		$('.answer').text(answer);
+		$('.number').text('');
+		$('.gem').each(function(){
+			var guess = Math.floor(Math.random() * 11) + 1;
+			$(this).attr('id', guess);
+		})
+	}
+	randNumGen();
+
+	$('.gem').on('click', function(){
+		var gemNum = parseInt($(this).attr('id'))
+		if (guessTotal + gemNum === answer) {
+			wins++;
+			randNumGen();
+			$('p.win').text('Wins: ' + wins);
+		} else if (guessTotal + gemNum > answer){
+			loss++;
+			randNumGen();
+			$('p.loss').text('Losses: ' + loss);
+		} else {
+			guessTotal += gemNum
+		}
+		$('.number').text(guessTotal);
+	});
+});
